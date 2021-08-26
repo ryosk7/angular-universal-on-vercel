00f222e9b5bb18a5b2010f9b0e9718df18e393b2
ryosk7 <uchiryo7@gmail.com>
2021-08-26 22:13:52 +0900

    fix(vercel): serverless functions crash fix

--- api/serverless.js
+++ api/serverless.js
@@ -1,3 +1,4 @@
 const server = require('../dist/ng-universal-on-vercel/server/main');
+const server = require(__dirname, './dist/ng-universal-on-vercel/server/main.js');
 
 module.exports = server.app();
