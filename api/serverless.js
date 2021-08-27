43c9d6758d1d8d2864d742b607429cbca9e173bd
ryosk7 <uchiryo7@gmail.com>
2021-08-27 16:28:17 +0900

    fix(vercel): fix path

--- api/serverless.js
+++ api/serverless.js
@@ -1,9 +1,3 @@
-import { environment } from './../src/environments/environment';
-
-if (environment.production) {
-  const server = require('../ng-universal-on-vercel/server/main');
-} else {
-  const server = require('../dist/ng-universal-on-vercel/server/main');
-}
+const server = require('../dist/ng-universal-on-vercel/server/main');
 
 module.exports = server.app();
